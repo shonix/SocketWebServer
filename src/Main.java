5fed1c5c5171c2018a82bd7d6c9cc6097c052f3e
Peter Raasthoej <peterfut123@gmail.com>
2015-09-24 20:02:57 +0200

    Errything works and is commented. We are now sending pictures of ourselves, to ourselves.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 
 public class Main {
 	public static void main(String[]args){
-		new Webserver();
+		try {
+			new Webserver();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/Webserver.java
+++ src/Webserver.java
@@ -1,32 +1,28 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+
 public class Webserver {
-	public Webserver(){
+	public Webserver()throws Exception{
+		//initialisere socket listener, og socket for users
 		ServerSocket socketListener;
 		Socket userSocket = null;
-		try {
-			socketListener = new ServerSocket(5559);
-			userSocket = socketListener.accept();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 		
-	if(userSocket != null){
-		try {
-			processGET(userSocket);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		}
+		//tilknytter port til socket listener
+		socketListener = new ServerSocket(5559);
+		//opretter user socket, ud fra accepteret socket listener
+		//kalder processGET med userSocket
+		userSocket = socketListener.accept();
+		if(userSocket != null){
+				processGET(userSocket);
+			}
+		socketListener.close(); //når operation er færdig, lukkes socketListener
 	}
 	public void processGET(Socket userSocket) throws IOException{
 		//Læser client input, altså input fra userSocket (det man skriver i adresse bar)
@@ -39,25 +35,24 @@ public class Webserver {
 		String[] requestParam = request.split(" ");
 		String path = "F:\\Brugere\\Shonix\\Pictures" + requestParam[1];
 		System.out.println("We want file: " + path);
-		
-		//Printwriter, bruges til output til client
-		PrintWriter out = new PrintWriter(userSocket.getOutputStream(),true);
-		
+				
 		//opretter ny fil, med path der blev modtaget fra client
 		File file = new File(path);
 		InputStream outToClient = new FileInputStream(file);
-		if(!file.exists()){
-			System.out.println("shit: ");
-			out.write("HTTP 404 shit \r\n\r\n");
-		}
 		int len = (int) file.length();
-		byte[] byteBuf = new byte[8000];
+		
+		//sætter filen i kø, for ikke at skulle sende hele fil på samme tid, sender den lidt ad gangen.. therefore buffer....
+		byte[] byteBuf = new byte[1024];
+		
+		//sætter det output, som den der er connected med socket får, til at være lig med en outputstream. I denne output stream, bliver der skrevet
+		//bytes, til, som kommer fra filen der ønskes at sendes. byteBuf står for at buffe bytesne der udgør filen, som der bliver sendt
+		//igennem socketed til client.
 		OutputStream fileOutput = userSocket.getOutputStream();
 		while((len = outToClient.read(byteBuf))!= -1){
 			fileOutput.write(byteBuf,0, len);
 		}
 		System.out.println(byteBuf);
 		inFromClient.close();
-		out.close();
+		outToClient.close();
 	}
 }
