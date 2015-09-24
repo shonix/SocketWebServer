3172a8435e0a14ea1571aec3832e3d8fe08b0e68
Peter Raasthoej <peterfut123@gmail.com>
2015-09-25 01:35:33 +0200

    Response med code 200 OK type works, generally errything works yo.

--- src/Webserver.java
+++ src/Webserver.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -7,24 +8,36 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Webserver {
+	//Fields
+
 	public Webserver()throws Exception{
 		//initialisere socket listener, og socket for users
 		ServerSocket socketListener;
 		Socket userSocket = null;
+		boolean running = true;
 		
-		//tilknytter port til socket listener
-		socketListener = new ServerSocket(5559);
-		//opretter user socket, ud fra accepteret socket listener
-		//kalder processGET med userSocket
-		userSocket = socketListener.accept();
-		if(userSocket != null){
+		//Sætter listenerern til at lytte på port 5555
+		socketListener = new ServerSocket(5557);
+		
+		while (running){
+			//opretter user socket, ud fra accepteret socket listener
+			//kalder processGET med userSocket
+			userSocket = socketListener.accept();
+			if(userSocket != null){
 				processGET(userSocket);
 			}
-		socketListener.close(); //når operation er færdig, lukkes socketListener
+		}
 	}
+	
+	@SuppressWarnings("deprecation")
 	public void processGET(Socket userSocket) throws IOException{
+		byte[] msgHandle;
+		String msg;
 		//Læser client input, altså input fra userSocket (det man skriver i adresse bar)
 		BufferedReader inFromClient = new BufferedReader(new InputStreamReader(userSocket.getInputStream()));
 		String request = inFromClient.readLine();
@@ -33,26 +46,57 @@ public class Webserver {
 		//opretter et string array hvori fil navn placeres, og sendes ind i en string (path)
 		//Splitter string der fås fra inFromClient op ved mellemrum -> GET | path | Protocol
 		String[] requestParam = request.split(" ");
-		String path = "F:\\Brugere\\Shonix\\Pictures" + requestParam[1];
+		if(!requestParam[0].equals("GET")){
+			userSocket.close();
+		}
+		if(requestParam[1].equals("/")){
+			requestParam[1] = "/index.html";
+		}
+		String path = "F:\\Brugere\\Shonix\\Pictures\\Hjemmeside" + requestParam[1];
 		System.out.println("We want file: " + path);
 				
 		//opretter ny fil, med path der blev modtaget fra client
 		File file = new File(path);
-		InputStream outToClient = new FileInputStream(file);
-		int len = (int) file.length();
-		
-		//sætter filen i kø, for ikke at skulle sende hele fil på samme tid, sender den lidt ad gangen.. therefore buffer....
-		byte[] byteBuf = new byte[1024];
-		
-		//sætter det output, som den der er connected med socket får, til at være lig med en outputstream. I denne output stream, bliver der skrevet
-		//bytes, til, som kommer fra filen der ønskes at sendes. byteBuf står for at buffe bytesne der udgør filen, som der bliver sendt
-		//igennem socketed til client.
-		OutputStream fileOutput = userSocket.getOutputStream();
-		while((len = outToClient.read(byteBuf))!= -1){
-			fileOutput.write(byteBuf,0, len);
+		//Finder fil extension ud fra filens path, til brug ved HEADER
+	    
+		OutputStream userOutput = userSocket.getOutputStream();
+		if(!file.exists()){
+			msg = "HTTP/1.1 404 FileNotFound";
+			msgHandle = msg.getBytes();
+			userOutput.write(msgHandle);
+			userSocket.close();
+			return;
 		}
-		System.out.println(byteBuf);
+		Path path2 = FileSystems.getDefault().getPath(path);
+		StringBuilder header = new StringBuilder();
+		addHeader(header, "HTTP/1.1 200 OK");
+		addHeader(header, "Connection close");
+		addHeader(header, "Content-Type: " + Files.probeContentType(path2));
+		addHeader(header, "File size: " +file.length());
+		addHeader(header,"");
+		String strHead = header.toString();
+		int transferedBytes = 0;
+		byte[] byteBuf = new byte[1024]; //sætter filen i kø, for ikke at skulle sende hele fil på samme tid, sender den lidt ad gangen.. therefore buffer....
+		while(transferedBytes < strHead.length()){ //Så længe al data fra strHead ikke er transfered, skal loop køre
+			strHead.getBytes(transferedBytes, Math.min(byteBuf.length,strHead.length()-transferedBytes), byteBuf, 0); //Starter på transfered bytes pointer (0 til start da intet flyttet) flytter 1024 bytes ad gangen, flytter til byte buffer, fra index 0.
+			userOutput.write(byteBuf,0,Math.min(strHead.length()-transferedBytes,byteBuf.length)); 	//vælger data der skal skrives til client socket. Skriver al data
+			transferedBytes+=byteBuf.length;														//ud fra index 0 til, hvad enten der er mindst ad bytebufferen, eller Headeres resterende størrelse
+		}
+		
+		InputStream outToClient = new FileInputStream(file);
+		while((transferedBytes = outToClient.read(byteBuf))!= -1){ 	//Sender bytes til user socket, så der er noget i byteBuf at læse, så snart bufferen er færdig, stopper while loopet
+			userOutput.write(byteBuf,0, transferedBytes);
+		}
+		//Lukker streams
 		inFromClient.close();
 		outToClient.close();
+		//lukker socket
+		userSocket.close();
+	}
+	
+	//Bruges til at bygge Header op for return
+	public void addHeader(StringBuilder header, String append){
+		header.append(append);
+		header.append("\r\n");
 	}
 }
